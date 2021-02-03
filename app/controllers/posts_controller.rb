0c32ee3bb0d3ef33b0d74145b5029e6788b16c4e
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2021-02-03 23:04:08 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,12 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create(params[:id])
+    redirect_to post
   end
 
   def edit
@@ -15,5 +18,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
